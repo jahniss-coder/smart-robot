7854ad3595c10fbac524e9f8e2ad66f77c764541
Jahnis <jahnis.schetrit@insa-rouen.fr>
2025-12-05 15:53:34 +0100

    bibliothèque configuration_GPIO.h

---
+++ configuration_GPIO.h
@@ -0,0 +1,44 @@
+/** 
+Bibliotèque pour le projet
+Celle-ci contient l'ensemble des fonctions utiles pour le projet Smart Robot ce qui inclut :
+    - L'initalisation des modes des GPIO
+    - L'initialisation des GPIO
+
+**/
+
+#ifndef __GPIO__
+#define __GPIO__
+
+// Moteur
+#define MOTEUR_G_PWM 12
+#define MOTEUR_D_PWM 13
+#define MOTEUR_G_IN1 24
+#define MOTEUR_G_IN2 25
+#define MOTEUR_D_IN3 25
+#define MOTEUR_D_IN4 6
+// Capteurs suiveurs lignes
+#define SUIVEUR_ ?
+#define SUIVEUR_ ?
+#define SUIVEUR_ ?
+#define SUIVEUR_ ?
+// Capteur couleur 
+#define COULEUR_ ?
+#define COULEUR_ ?
+// Buzzeur
+#define BUZZER ?
+// LCD
+#define LCD ?
+#define LCD ?
+
+/**
+    @brief Initialiser le Mode des GPIO (OUTPUT ou INPUT)
+**/
+void GPIO_configurerModeGpio();
+
+/**
+    @brief Initialise les GPIO
+    @return 0 si succès, -1 si erreur
+**/
+int GPIO_initialiserGpio();
+
+#endif
