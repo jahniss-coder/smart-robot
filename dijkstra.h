6b7c52920890250afa6b39b4744cb52c3973302d
nolivier <neil.olivier@insa-rouen.fr>
2025-12-11 21:24:33 +0100

    Modification des types int -> unsigned int pour la cohérence

--- dijkstra.h
+++ dijkstra.h
@@ -9,17 +9,17 @@
 
 // On va stocker le chemin que l'on veut que notre robot suive
 typedef struct{
-    int points[NB_POINTS_MAX];  // Tableau ou on va stocker nos points par lesquels le robot doit passer
-    int nb_points;  // Nombre de points que l'on a dans notre tableau de points
+    unsigined int points[NB_POINTS_MAX];  // Tableau ou on va stocker nos points par lesquels le robot doit passer
+    unsigined int nb_points;  // Nombre de points que l'on a dans notre tableau de points
     //int nb_pas; // Correspond a la longueur -1 (aussi nombre d'arrêtes)
 } Chemin;
 
 // Structure pour donner toutes les informations
 typedef struct{
-    int point_actuel;   // Le point sur lequel on se trouve
-    int point_precedent;    // Le point depuis lequel on arrive
-    int chemin[NB_POINTS_MAX];      // Tous les points par lesquels il faut passer pour arriver jusqu'au point actuel
-    int nb_points;    // Le nombre de points dans le chemin
+    unsigned int point_actuel;   // Le point sur lequel on se trouve
+    unsigned int point_precedent;    // Le point depuis lequel on arrive
+    unsigned int chemin[NB_POINTS_MAX];      // Tous les points par lesquels il faut passer pour arriver jusqu'au point actuel
+    unsigned int nb_points;    // Le nombre de points dans le chemin
 } EtatDuDijkstra;
 
 typedef struct{
@@ -33,8 +33,8 @@ typedef struct{
     P3 -> P1 -> P2
     P3 -> P2 -> P1
     **/
-    int chemin_complet[NB_POINTS_MAX];  // Tableau avec tous les points par lesquels on doit passer
-    int longueur_chemin_complet;
+    unsigned int chemin_complet[NB_POINTS_MAX];  // Tableau avec tous les points par lesquels on doit passer
+    unsigned int longueur_chemin_complet;
 }Solution;
 
 /**
@@ -44,7 +44,7 @@ typedef struct{
    @param arrivee: Point ou l'on souhaite arriver
    @return un chemin, le chemin le plus court
 **/
-Chemin dijkstra(Graphe* g, int depart, int arrivee);
+Chemin dijkstra(Graphe* g, unsigned int depart, unsigned int arrivee);
 
 
 /**
@@ -55,14 +55,14 @@ Chemin dijkstra(Graphe* g, int depart, int arrivee);
    @param longueur_ville longueur de la ville
    @return la solution du chemin le plus court
 **/
-Solution resoudre_chemin_plus_court(Graphe* g, int point_obligatoires[3], int depart, int longueur_ville);
+Solution resoudre_chemin_plus_court(Graphe* g, unsigned int point_obligatoires[3], unsigned int depart, unsigned int longueur_ville);
 
 /**
    @brief pouvoir permuter l'ordre dans lequel on passe aux points
    @param arr: tableau 1D ou l'on va stocker les 3 points ou l'on se trouve
    @param permutations: tableau 2D pour stocker les 6 possibilités
 **/
-void permutation(int arr[3], int permutations[6][3]);
+void permutation(unsigned int arr[3], unsigned int permutations[6][3]);
 
 
 
