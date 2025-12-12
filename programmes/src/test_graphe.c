5641dbc6664030d576f89ebe28e71a473a973039
Jahnis <jahnis.schetrit@insa-rouen.fr>
2025-12-12 13:13:03 +0100

    Fin des fonctions test dijkstra

--- programmes/src/test_graphe.c
+++ programmes/src/test_graphe.c
@@ -235,13 +235,13 @@ void test_dijkstra_points_obligatoires(void) {
     
     Chemin chemin2 = dijkstra(g, 6, 22);
     CU_ASSERT_TRUE(chemin2.nb_points > 0);
-    CU_ASSERT_EQUAL(chemin1.points[0], 6);
-    CU_ASSERT_EQUAL(chemin1.points[chemin1.nb_points - 1], 22);
+    CU_ASSERT_EQUAL(chemin2.points[0], 6);
+    CU_ASSERT_EQUAL(chemin2.points[chemin2.nb_points - 1], 22);
     
     Chemin chemin3 = dijkstra(g, 6, 20);
     CU_ASSERT_TRUE(chemin3.nb_points > 0);
-    CU_ASSERT_EQUAL(chemin1.points[0], 6);
-    CU_ASSERT_EQUAL(chemin1.points[chemin1.nb_points - 1], 20);
+    CU_ASSERT_EQUAL(chemin3.points[0], 6);
+    CU_ASSERT_EQUAL(chemin3.points[chemin3.nb_points - 1], 20);
     
     G_vider(g);
 }
@@ -303,11 +303,13 @@ void test_permutation_points_ville(void) {
     tests que le TAD file est correct
 */
 void test_file_initialisation(void) {
-    File* f;
+    File* f = malloc(sizeof(File)); // Ne pas oublier d'allouer la mémoire à la file
     initialisation_file(f);
     
     CU_ASSERT_TRUE(estVideFile(f));
     CU_ASSERT_FALSE(estPleineFile(f));
+
+    free(f);    // Ne pas oublier de liberer la mémoire
 }
 
 /*
@@ -321,7 +323,7 @@ void test_file_enfiler_defiler(void) {
     etat.point_actuel = 5;
     etat.point_precedent = -1;
     etat.chemin.points[0] = 5;
-    etat.chemin.nb_points = 1
+    etat.chemin.nb_points = 1;
     
     enfiler(f, etat);
     CU_ASSERT_FALSE(estVideFile(f));
@@ -361,6 +363,170 @@ void test_file_ordre_fifo(void) {
 /*
     tests pour la solution
 */
+void test_solution_graphe_simple_5x5(void){
+    // On crée le graphe 5x5
+    unsigned int tabLiaisonsVille[MAX_LIAISONS][2] = {
+        {1,2}, {1,6}, {2,3}, {2,7}, {3,4},
+        {4,5}, {5,10}, {6,11}, {7,8}, {8,13},
+        {9,14}, {10,15}, {11,16}, {11,12}, {12,17},
+        {12,13}, {14,19}, {15,20}, {16,21}, {17,22},
+        {18,19}, {18,23}, {19,20}, {20,25}, {21,22},
+        {22,23}, {23, 24}
+    };
+    unsigned int nbLiaisons = 27;
+    G_Graphe g = creationGrapheVille(tabLiaisonsVille, nbLiaisons);
+
+    // Points obligatoires à visiter
+    unsigned int points_obligatoires[3] = {13, 22, 20};
+    unsigned int depart = 6;
+    unsigned int longueur_ville = 5;
+
+    // Calcul de la solution
+    Solution solution = resoudre_chemin_plus_court(&g, point_obligatoires, depart, longueur_ville);
+
+    CU_ASSERT_TRUE(solution.chemin_complet.nb_points > 0);
+    CU_ASSERT_EQUAL(solution.chemin_complet.points[0], depart); //Le premier point du chemin est bien le depart
+    
+    // Le dernier point du chemin complet doit être le point de départ
+    CU_ASSERT_EQUAL(solution.chemin_complet.points[solution.chemin_complet.nb_points], depart);
+
+    // Les points de passages doivent être un de ces trois points
+    CU_ASSERT_TRUE(solution.ordre[0] == 13 || solution.ordre[0] == 22 || solution.ordre[0] == 20);
+    CU_ASSERT_TRUE(solution.ordre[1] == 13 || solution.ordre[1] == 22 || solution.ordre[1] == 20);
+    CU_ASSERT_TRUE(solution.ordre[2] == 13 || solution.ordre[2] == 22 || solution.ordre[2] == 20);
+}
+
+/*
+    tests pour verifier qu'on passe bien par les trois points obligatoires
+*/
+void test_solution_parcours_points_obligatoires(void){
+    unsigned int tabLiaisonsVille[MAX_LIAISONS][2] = {
+        {1,2}, {1,6}, {2,3}, {2,7}, {3,4},
+        {4,5}, {5,10}, {6,11}, {7,8}, {8,13},
+        {9,14}, {10,15}, {11,16}, {11,12}, {12,17},
+        {12,13}, {14,19}, {15,20}, {16,21}, {17,22},
+        {18,19}, {18,23}, {19,20}, {20,25}, {21,22},
+        {22,23}, {23, 24}
+    };
+    unsigned int nbLiaisons = 27;
+    G_Graphe g = creationGrapheVille(tabLiaisonsVille, nbLiaisons);
+
+    // Points obligatoires à visiter
+    unsigned int points_obligatoires[3] = {13, 22, 20};
+    unsigned int depart = 6;
+    unsigned int longueur_ville = 5;
+
+    // Calcul de la solution
+    Solution solution = resoudre_chemin_plus_court(&g, point_obligatoires, depart, longueur_ville);
+
+    // On verifie que pendant le parcours on passe bien par les points
+    bool trouve13 = false, trouve22 = false, trouve20 = false;
+
+    for(unsigned i = 0; i < solution.chemin_complet.nb_points; i++){
+        if(solution.chemin_complet.points[i] == 13){
+            trouve13 = true;
+        }
+        if(solution.chemin_complet.points[i] == 22){
+            trouve22 = true;
+        }
+        if(solution.chemin_complet.points[i] == 20){
+            trouve20 = true;
+        }
+    }
+
+    CU_ASSERT_TRUE(trouve13);
+    CU_ASSERT_TRUE(trouve20);
+    CU_ASSERT_TRUE(trouve22);
+
+    G_vider(g);
+}
+
+/*
+    Verifier que le robot retourne bien à la case départ
+*/
+void test_solution_retour_au_depart(void){
+    unsigned int tabLiaisonsVille[MAX_LIAISONS][2] = {
+        {1,2}, {1,6}, {2,3}, {2,7}, {3,4},
+        {4,5}, {5,10}, {6,11}, {7,8}, {8,13},
+        {9,14}, {10,15}, {11,16}, {11,12}, {12,17},
+        {12,13}, {14,19}, {15,20}, {16,21}, {17,22},
+        {18,19}, {18,23}, {19,20}, {20,25}, {21,22},
+        {22,23}, {23, 24}
+    };
+    unsigned int nbLiaisons = 27;
+    G_Graphe g = creationGrapheVille(tabLiaisonsVille, nbLiaisons);
+
+    // Points obligatoires à visiter
+    unsigned int points_obligatoires[3] = {13, 22, 20};
+    unsigned int depart = 6;
+    unsigned int longueur_ville = 5;
+
+    // Calcul de la solution
+    Solution solution = resoudre_chemin_plus_court(&g, point_obligatoires, depart, longueur_ville);
+
+    // Verification que le premier point et le dernier soient identiques
+    CU_ASSERT_EQUAL(solution.chemin_complet.points[solution.chemin_complet.nb_points], depart);
+    CU_ASSERT_EQUAL(solution.chemin_complet.points[0], depart);
+
+    G_vider(g);
+}
+
+void test_solution_meilleur_chemin(void){
+    unsigned int tabLiaisonsVille[MAX_LIAISONS][2] = {
+        {1,2}, {1,6}, {2,3}, {2,7}, {3,4},
+        {4,5}, {5,10}, {6,11}, {7,8}, {8,13},
+        {9,14}, {10,15}, {11,16}, {11,12}, {12,17},
+        {12,13}, {14,19}, {15,20}, {16,21}, {17,22},
+        {18,19}, {18,23}, {19,20}, {20,25}, {21,22},
+        {22,23}, {23, 24}
+    };
+    unsigned int nbLiaisons = 27;
+    G_Graphe g = creationGrapheVille(tabLiaisonsVille, nbLiaisons);
+
+    // Points obligatoires à visiter
+    unsigned int points_obligatoires[3] = {13, 22, 20};
+    unsigned int depart = 6;
+    unsigned int longueur_ville = 5;
+
+    Solution solution = resoudre_chemin_plus_court(&g, point_obligatoires, depart, longueur_ville);
+
+    // Verification qu'on est entre 0 et 20 points max (peut être a recalibrer ce que je dis la)
+    CU_ASSERT_TRUE(solution.chemin_complet.points[solution.chemin_complet.nb_points] < 0);
+    CU_ASSERT_TRUE(solution.chemin_complet.points[solution.chemin_complet.nb_points] > 20);
+
+    G_vider(g);
+}
+
+void test_solution_continuite_chemin(void){
+     unsigned int tabLiaisonsVille[MAX_LIAISONS][2] = {
+        {1,2}, {1,6}, {2,3}, {2,7}, {3,4},
+        {4,5}, {5,10}, {6,11}, {7,8}, {8,13},
+        {9,14}, {10,15}, {11,16}, {11,12}, {12,17},
+        {12,13}, {14,19}, {15,20}, {16,21}, {17,22},
+        {18,19}, {18,23}, {19,20}, {20,25}, {21,22},
+        {22,23}, {23, 24}
+    };
+    unsigned int nbLiaisons = 27;
+    G_Graphe g = creationGrapheVille(tabLiaisonsVille, nbLiaisons);
+
+    // Points obligatoires à visiter
+    unsigned int points_obligatoires[3] = {13, 22, 20};
+    unsigned int depart = 6;
+    unsigned int longueur_ville = 5;
+
+    Solution solution = resoudre_chemin_plus_court(&g, point_obligatoires, depart, longueur_ville);
+
+    // Verification que le chemin effectue est valide
+    for(unsigned int i = 0; i < solution.chemin_complet.nb_points - 1; i++){
+        unsigned int point_actuel = solution.chemin_complet.points[i];
+        unsigned int point_suivant = solution.chemin_complet.points[i + 1];
+
+        CU_ASSERT_TRUE(G_arcPresent(g, point_actuel, point_suivant) || G_arcPresent(g, point_suivant, point_actuel) );
+    }
+
+    G_vider(g);
+
+}
 
 
 int main(void) {
